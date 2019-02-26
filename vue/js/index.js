4a49207b80d5ed8995642fc8368f3620197beccf
amoswish <1572912653@qq.com>
2019-02-26 17:05:41 +0800

    完成主界面编写及跳转

--- vue/js/index.js
+++ vue/js/index.js
@@ -1,6 +1,44 @@
 new Vue({
     el: '#app',
     data:{
-        backgroundimg:'../static/res/backgroundimg/backgroudimg.jpg',
-    }
+        backgroundimg:'../static/res/backgroundimg/backgroudimgheight.jpg',
+        screenwidth:100,
+        screenheight:100,
+    },
+    computed:{
+        widthchange:function(){
+            return this.screenwidth;
+        },
+        heightchange:function(){
+            return this.screenheight;
+        },
+    },
+    mounted () {
+        //适配主页背景图片格式
+        this.screenwidth = window.innerWidth;
+        this.screenheight = window.innerHeight;
+        if(this.screenwidth/this.screenheight>1){
+            this.backgroundimg = "../static/res/backgroundimg/backgroundimgwidth.jpeg"
+        }
+        else{
+            this.backgroundimg = '../static/res/backgroundimg/backgroudimgheight.jpg'
+        }
+    },
+    methods: { 
+        //适配主页背景图片格式
+        resize () {
+            this.screenwidth = window.innerWidth;
+            this.screenheight = window.innerHeight;
+            if(this.screenwidth/this.screenheight>1){
+                this.backgroundimg = "../static/res/backgroundimg/backgroundimgwidth.jpeg"
+            }
+            else{
+                this.backgroundimg = '../static/res/backgroundimg/backgroudimgheight.jpg'
+            }
+        },
+        tomainpage(){
+            var url =  window.location.href;
+            window.location.href=url.split("/index.html")[0]+"/main_html/main.html";
+        }
+    },
 });
